0774d548134ce37561bb8b0d98acff0d28d20467
karlasafa <karlasafa20@gmail.com>
2022-07-30 23:20:38 +0700

    styling

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { KonsumenComponent } from './components/konsumen/konsumen.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductComponent } from './components/product/product.component';
+import { RekeningComponent } from './components/rekening/rekening.component';
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
   {path:'login',component:LoginComponent},
   {path: 'product',component:ProductComponent},
+  {path: 'admin',component:AdminComponent},
+  {path: 'konsumen',component:KonsumenComponent},
+  {path: 'rekening',component:RekeningComponent},
   
 ];
 
