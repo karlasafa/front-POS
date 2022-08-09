9e66e5026bde3fef0a9faef4377039c1c505a069
kyleesandra <kyleesandra1104@gmail.com>
2022-08-09 22:53:02 +0700

    try

--- angular-front/src/app/components/product/product.component.ts
+++ angular-front/src/app/components/product/product.component.ts
@@ -7,11 +7,31 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  props : any;
+  btnprops1: any;
+  warna: any;
 
   constructor(private judulService: Title) { }
 
   ngOnInit(): void {
     this.judulService.setTitle('Product Page');
+    this.props='block';
+    this.btnprops1='block';
+    this.warna='red'
+  }
+
+  Muncul(states:any){
+    if(states === 'block'){
+      this.props = states;
+      this.btnprops1 = 'none'
+      this.warna = 'red'
+
+    }
+    else{
+      this.props = states;
+      this.btnprops1 ='block'
+      this.warna ='green'
+    }
   }
 
 }
