d0b849069ab5b50fc5139c0f89c1b5d2337f1c77
kyleesandra <kyleesandra1104@gmail.com>
2022-08-10 23:30:22 +0700

    cobain

--- angular-front/src/app/components/admin/admin.component.ts
+++ angular-front/src/app/components/admin/admin.component.ts
@@ -7,11 +7,28 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  props : any;
+  btnprops1 : any;
 
   constructor(private judulService: Title) { }
 
   ngOnInit(): void {
     this.judulService.setTitle('Admin Page');
+    this.props='block'
+    this.btnprops1='block'
+
+  }
+
+  Muncul(state:any){
+
+    if(state === 'block'){
+      this.props = state;
+      this.btnprops1 = 'none';
+    }
+    else{
+      this.props = state;
+      this.btnprops1 = 'block';
+    }
   }
 
 }
--- angular-front/src/app/components/rekening/rekening.component.ts
+++ angular-front/src/app/components/rekening/rekening.component.ts
@@ -7,11 +7,27 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./rekening.component.css']
 })
 export class RekeningComponent implements OnInit {
+  props : any;
+  btnprops1 : any;
 
   constructor(private judulService: Title) { }
 
   ngOnInit(): void {
     this.judulService.setTitle('Rekening Page');
+    this.props='block'
+    this.btnprops1='block'
+
+  }
+  Muncul(state:any){
+
+    if(state === 'block'){
+      this.props = state;
+      this.btnprops1 = 'none';
+    }
+    else{
+      this.props = state;
+      this.btnprops1 = 'block';
+    }
   }
 
 }
