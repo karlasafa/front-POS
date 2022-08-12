d2a7e0725f5cdb1592818346573b704dd6be2ba3
kyleesandra <kyleesandra1104@gmail.com>
2022-08-12 09:20:43 +0700

    coba

--- angular-front/src/app/components/rekening/rekening.component.ts
+++ angular-front/src/app/components/rekening/rekening.component.ts
@@ -9,13 +9,16 @@ import { Title } from '@angular/platform-browser';
 export class RekeningComponent implements OnInit {
   props : any;
   btnprops1 : any;
+  userloggedIn = true;
+  btnprops2: any;
 
   constructor(private judulService: Title) { }
 
   ngOnInit(): void {
     this.judulService.setTitle('Rekening Page');
-    this.props='block'
-    this.btnprops1='block'
+    this.props='block';
+    this.btnprops1='block';
+    this.btnprops2='none';
 
   }
   Muncul(state:any){
@@ -23,9 +26,11 @@ export class RekeningComponent implements OnInit {
     if(state === 'block'){
       this.props = state;
       this.btnprops1 = 'none';
+      this.btnprops2 = state
     }
     else{
       this.props = state;
+      this.btnprops2 = 'none';
       this.btnprops1 = 'block';
     }
   }
